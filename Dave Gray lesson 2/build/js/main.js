b02628bc646878821b3c719bdee1c8b4953d572e
VansRouges <evansagina57@gmail.com>
2023-04-29 17:28:44 +0100

    lesson 1 completed

---
+++ Dave Gray lesson 2/build/js/main.js
@@ -0,0 +1,15 @@
+"use strict";
+let myName = 'Vance';
+let meaningOfLife;
+let isLoading;
+let album;
+myName = 'Michael';
+meaningOfLife = 42;
+isLoading = true;
+album = 1984;
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let re = /w+/g;
---
+++ Dave Gray lesson 2/src/main.ts
@@ -0,0 +1,18 @@
+let myName: string = 'Vance'
+let meaningOfLife: number;
+let isLoading: boolean;
+let album: string | number;
+
+myName = 'Michael'
+meaningOfLife = 42
+isLoading = true
+album = 1984
+
+const sum = (a:number, b:string) => {
+    return a + b
+}
+
+let postId: string | number
+let isActive: number | boolean
+
+let re: RegExp = /w+/g
